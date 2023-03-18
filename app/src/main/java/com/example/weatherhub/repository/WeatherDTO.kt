b6e154417f6bcbb40c59827408ae80fde9206995
AlexanderPalatkin <palatkin.alexander@gmail.com>
2023-03-18 23:20:12 +0400

    Урок 6. Создал класс сервиса DetailsService.kt, подготовил структуру. Добавил ключи используемые в проекте в константы в классе Utils.kt

--- app/src/main/java/com/example/weatherhub/repository/WeatherDTO.kt
+++ app/src/main/java/com/example/weatherhub/repository/WeatherDTO.kt
@@ -1,10 +1,13 @@
 package com.example.weatherhub.repository
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class WeatherDTO(
     @SerializedName("fact")
     val factDTO: FactDTO,
     @SerializedName("info")
     val infoDTO: InfoDTO
-)
+):Parcelable
