614aa444b644b93529856f0ea328e78c72d8de41
AlexanderPalatkin <palatkin.alexander@gmail.com>
2023-03-30 00:58:05 +0400

    Урок 9. Добавил запрос на получение permission для чтения контактов

--- app/src/main/java/com/example/weatherhub/lesson9/WorkWithContentProviderFragment.kt
+++ app/src/main/java/com/example/weatherhub/lesson9/WorkWithContentProviderFragment.kt
@@ -1,11 +1,13 @@
 package com.example.weatherhub.lesson9
 
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.weatherhub.R
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.weatherhub.databinding.FragmentWorkWithContentProviderBinding
 
 class WorkWithContentProviderFragment : Fragment() {
@@ -20,9 +22,71 @@ class WorkWithContentProviderFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentWorkWithContentProviderBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    val REQUEST_CODE = 42
+    private fun checkPermission() {
+//       есть ли разрешение
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            getContacts()
+        } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.READ_CONTACTS)) {
+            // важно написать убедительную просьбу
+            explain()
+        } else {
+            mRequestPermission()
+        }
+    }
+
+    private fun mRequestPermission() {
+        requestPermissions(arrayOf(android.Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.isNotEmpty()
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                getContacts()
+            } else {
+                explain()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun getContacts() {
+
+    }
+
+    private fun explain() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Доступ к контактам")
+            .setMessage("Объяснение")
+            .setPositiveButton("Предоставить доступ") { _, _ ->
+                mRequestPermission()
+            }
+            .setNegativeButton("Не надо") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
     }
 
     companion object {
