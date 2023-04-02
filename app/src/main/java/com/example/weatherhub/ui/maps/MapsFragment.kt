6dd4176eb4aede28a99dc6aceb3eaf8d1caf9790
AlexanderPalatkin <palatkin.alexander@gmail.com>
2023-04-02 14:47:52 +0400

    Урок 9. Добавил маркеры на карту и возможность проложить маршрут между этими маркерами

--- app/src/main/java/com/example/weatherhub/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/example/weatherhub/ui/maps/MapsFragment.kt
@@ -1,5 +1,8 @@
 package com.example.weatherhub.ui.maps
 
+import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,14 +11,16 @@ import androidx.fragment.app.Fragment
 import com.example.weatherhub.R
 import com.example.weatherhub.databinding.FragmentMapsMainBinding
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import kotlinx.android.synthetic.main.fragment_maps_main.*
+import java.io.IOException
 
 class MapsFragment : Fragment() {
 
-    private var _binding : FragmentMapsMainBinding? = null
+    private var _binding: FragmentMapsMainBinding? = null
     private val binding get() = _binding!!
 
     override fun onDestroy() {
@@ -33,10 +38,21 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val moscow = LatLng(55.0, 37.0)
-        googleMap.addMarker(MarkerOptions().position(moscow).title("Marker in Moscow"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(moscow))
+        map = googleMap
+        val initialPlace = LatLng(52.52000659999999, 13.404953999999975)
+        googleMap.addMarker(
+            MarkerOptions().position(initialPlace).title(getString(R.string.marker_start))
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(initialPlace))
+        googleMap.setOnMapLongClickListener { latLng ->
+            getAddressAsync(latLng)
+            addMarkerToArray(latLng)
+            drawLine()
+        }
     }
+
+    private lateinit var map: GoogleMap
+    private val markers: ArrayList<Marker> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,5 +67,102 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        initSearchByAddress()
     }
+
+    private fun getAddressAsync(location: LatLng) {
+        context?.let {
+            val geoCoder = Geocoder(it)
+            Thread {
+                try {
+                    val addresses =
+                        geoCoder.getFromLocation(
+                            location.latitude,
+                            location.longitude, 1
+                        )
+                    textAddress.post {
+                        textAddress.text =
+                            addresses?.get(0)?.getAddressLine(0)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
+
+    private fun addMarkerToArray(location: LatLng) {
+        val marker = setMarker(
+            location, markers.size.toString(),
+            R.drawable.ic_map_pin
+        )
+        marker?.let { markers.add(it) }
+    }
+
+    private fun setMarker(
+        location: LatLng,
+        searchText: String,
+        resourceId: Int
+    ): Marker? {
+        return map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(searchText)
+                .icon(BitmapDescriptorFactory.fromResource(resourceId))
+        )
+    }
+
+    private fun drawLine() {
+        val last: Int = markers.size - 1
+        if (last >= 1) {
+            val previous: LatLng = markers[last - 1].position
+            val current: LatLng = markers[last].position
+            map.addPolyline(
+                PolylineOptions()
+                    .add(previous, current)
+                    .color(Color.RED)
+                    .width(5f)
+            )
+        }
+    }
+
+    private fun initSearchByAddress() {
+        binding.buttonSearch.setOnClickListener {
+            val geoCoder = Geocoder(it.context)
+            val searchText = searchAddress.text.toString()
+            Thread {
+                try {
+                    val addresses = geoCoder.getFromLocationName(searchText, 1)
+                    if (addresses != null) {
+                        if (addresses.size > 0) {
+                            goToAddress(addresses, it, searchText)
+                        }
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
+
+    private fun goToAddress(
+        addresses: MutableList<Address>,
+        view: View,
+        searchText: String
+    ) {
+        val location = LatLng(
+            addresses[0].latitude,
+            addresses[0].longitude
+        )
+        view.post {
+            setMarker(location, searchText, R.drawable.ic_map_marker)
+            map.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    location,
+                    15f
+                )
+            )
+        }
+    }
+
 }
