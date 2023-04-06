86590bc69911ec5be0641100e3f67dc6515b1d9e
AlexanderPalatkin <palatkin.alexander@gmail.com>
2023-04-06 23:44:38 +0400

    Урок 11. Добавил в приложение возможность получения push-уведомлений.

--- app/src/main/java/com/example/weatherhub/model/domain/usecase/DetailsRepositoryAll.kt
+++ app/src/main/java/com/example/weatherhub/model/domain/usecase/DetailsRepositoryAll.kt
@@ -1,6 +1,6 @@
 package com.example.weatherhub.model.domain.usecase
 
-import com.example.weatherhub.viewmodel.HistoryViewModel
+import com.example.weatherhub.ui.history.HistoryViewModel
 
 interface DetailsRepositoryAll {
     fun getAllWeatherDetails(callback: HistoryViewModel.CallBackForAllCities)
---
+++ app/src/main/java/com/example/weatherhub/service/MyFBService.kt
@@ -0,0 +1,58 @@
+package com.example.weatherhub.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.example.weatherhub.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFBService : FirebaseMessagingService() {
+    override fun onMessageReceived(message: RemoteMessage) {
+        if (message.data.isNotEmpty()) {
+            val title = message.data[KEY_TITLE]
+            val message = message.data[KEY_MESSAGE]
+            if (!title.isNullOrEmpty() && !message.isNullOrEmpty()) {
+                push(title, message)
+            }
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID_HIGH = 1
+        private const val CHANNEL_ID_HIGH = "channel_id_high"
+        private const val KEY_TITLE = "myTitle"
+        private const val KEY_MESSAGE = "myMessage"
+    }
+
+    private fun push(title: String, message: String) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+        val notificationBuilderTest = NotificationCompat.Builder(this, CHANNEL_ID_HIGH).apply {
+            setSmallIcon(R.drawable.ic_earth)
+            setContentTitle(title)
+            setContentText(message)
+            priority = NotificationManager.IMPORTANCE_HIGH
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Name $CHANNEL_ID_HIGH"
+            val channelDescription = "Description $CHANNEL_ID_HIGH"
+            val channelPriority = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID_HIGH, channelName, channelPriority).apply {
+                description = channelDescription
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(NOTIFICATION_ID_HIGH, notificationBuilderTest.build())
+    }
+}
